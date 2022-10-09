286ae9428a94f12d80784c5a7accee2cc736d620
mega-hedge <110251934+mega-hedge@users.noreply.github.com>
2022-10-09 19:46:13 +0500

    Update usd-rub.py

--- usd-rub.py
+++ usd-rub.py
@@ -1,16 +1,26 @@
-print('-' * 10, 'USD - RUB', '-' * 10) # print program name
+try: # Main code
 
-try:
+	# Import libraries
 	from requests import get # import lib for parsing
+	from colorama import Fore # import lib for color font
 	from time import sleep # import sleep func
 
-	for i in range(10000):
+	from os import system as sy # import lib for set win size
+	sy('mode 19, 2') # set start win size
+
+
+	while True: # Infinite while
 		rub = get( 'https://www.cbr-xml-daily.ru/daily_json.js' ).json() # parsing USD
-		print('1 USD =', round( rub['Valute']['USD']['Value'] , 3), 'RUB') # print USD to RUB
-		sleep(3)
+		print( f"{Fore.GREEN}1 USD = {Fore.RED}{ round( rub['Valute']['USD']['Value'] , 3) } {Fore.GREEN}RUB" ) # print USD to RUB
 
-except:
-	print('Error! Please, reset program!')
+		sleep(5) # delay 5 sec
+		sy('cls') # clear display
 
-else:
-	input()
+
+except: # If have errors
+	sy('python -m pip install -U pip')
+	sy('pip3 install colorama')
+	sy('pip3 install requests')
+
+	sy('mode 30, 3')
+	input('All libraries is installed. Please, reset this program')
