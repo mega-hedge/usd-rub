e0b4bc4dbd250c9d18a0dee011eac1e324456faa
mega-hedge <110251934+mega-hedge@users.noreply.github.com>
2022-10-13 21:14:47 +0500

    Update usd-rub.py

--- usd-rub.py
+++ usd-rub.py
@@ -1,28 +1,18 @@
-try: # Main code
+# Import libraries
+from requests import get # import lib for parsing
+from colorama import Fore # import lib for color font
+from time import sleep # import sleep func
 
-	usd = int( input('Enter usd: ') )
-
-	# Import libraries
-	from requests import get # import lib for parsing
-	from colorama import Fore # import lib for color font
-	from time import sleep # import sleep func
-
-	from os import system as sy # import lib for set win size
-	sy('mode 19, 2') # set start win size
+from os import system as sy # import lib for set win size
+sy('mode 24,2') # set start win size
 
 
-	while True: # Infinite while
-		rub = get( 'https://www.cbr-xml-daily.ru/daily_json.js' ).json() # parsing USD
-		print( f"{Fore.RED}{usd} {Fore.GREEN}USD = {Fore.RED}{ round( rub['Valute']['USD']['Value']  * usd , 3) } {Fore.GREEN}RUB" ) # print USD to RUB
+usd = int( input( f'{Fore.GREEN}Enter usd: {Fore.RED}') )
+sy('cls') # clear display
 
-		sleep(5) # delay 5 sec
-		sy('cls') # clear display
+while True: # Infinite while
+	rub = get( 'https://www.cbr-xml-daily.ru/daily_json.js' ).json() # parsing USD
+	print( f"{Fore.RED}{usd} {Fore.GREEN}USD = {Fore.RED}{ round( rub['Valute']['USD']['Value']  * usd , 3) } {Fore.GREEN}RUB" ) # print USD to RUB
 
-
-except: # If have errors
-	sy('python -m pip install -U pip')
-	sy('pip3 install colorama')
-	sy('pip3 install requests')
-
-	sy('mode 30, 3')
-	input('All libraries is installed. Please, reset this program')
+	sleep(5) # delay 5 sec
+	sy('cls') # clear display
